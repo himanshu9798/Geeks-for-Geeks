6be31124202a7971d18db794a0bfe3fc74c6e504
Himanshu kumar barnawal <129401939+himanshu9798@users.noreply.github.com>
2025-03-19 12:45:27 +0530

    Create Solution.js

---
+++ Day2/Solution.js
@@ -0,0 +1,13 @@
+// problem name:-Decision Making in Java
+class Solution {
+    public static String compareNM(int n, int m) {
+        if (n<m){
+            return "lesser";
+        } else if(n>m){
+            return "greater";
+        }
+        else{
+            return "equal";
+        }
+    }
+}
