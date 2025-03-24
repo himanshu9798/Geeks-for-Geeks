f64db056aaed76e5baca6b3fd4aa623eacfb2890
Himanshu kumar barnawal <129401939+himanshu9798@users.noreply.github.com>
2025-03-24 09:19:37 +0530

    Create larger.java

---
+++ day5/larger.java
@@ -0,0 +1,15 @@
+//Largest Element in Array
+class Solution {
+    public static int largest(int[] arr) {
+        int largest=Integer.MIN_VALUE;
+        
+       for(int i=0;i<arr.length;i++)
+       {
+           if(arr[i]>largest)
+           {
+               largest=arr[i];
+           }
+       }
+       return largest;
+    }
+}
